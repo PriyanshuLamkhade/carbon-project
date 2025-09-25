56a005888374d91d25c3e4d7934570401fb6898d
Priyanshu <priyanshulamkhade@gmail.com>
2025-09-25 23:07:15 +0530

    basic endpoints

---
+++ server/routes/adminRoute.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const adminRouter = express.Router();
+adminRouter.get('requestsPending',(req,res)=>{
+
+})
+
+adminRouter.get('requestInfo',(req,res)=>{
+    
+})
+
+adminRouter.post('verifyDataForm',(req,res)=>{
+    
+})
+adminRouter.post('confirmSubmission',(req,res)=>{
+    
+})
---
+++ server/routes/userRoutes.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const userRouter = express.Router();
+
+userRouter.get('history',(req,res)=>{
+
+})
+
+userRouter.get('data',(req,res)=>{
+    
+})
+
+userRouter.post('userForm',(req,res)=>{
+    
+})
